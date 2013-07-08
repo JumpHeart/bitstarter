753a884cc53cc1b3c1b65f4baf690bc9bdbc5531
xiao zan <zanxiaostreak@gmail.com>
2013-07-08 10:25:05 +0000

    read from file

--- web.js
+++ web.js
@@ -2,11 +2,22 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fileName = 'index.html';
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(readFromFile(fileName));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var readFromFile(fileName)
+{
+    fs = require('fs');
+    var bufferSize = 64;
+    var buffer  = new  Buffer(bufferSizej);
+    buffer = fs.readFileSync(fileName);
+    return buffer.toString('utf-8');
+}
